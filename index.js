6e1c20a7e3090e165cb28617d0dea7f9825e171d
linse2268 <161201558+linse2268@users.noreply.github.com>
2024-04-25 15:31:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var character = document.getElementById('character');
 var block = document.getElementById('block');
 var text = document.getElementById('text');
+var viewportWidth =  parseInt(window.innerWidth);
 var running = true;
+var score = document.getElementById('score');
 
 
 
@@ -26,17 +28,36 @@ var checkdead = setInterval(function () {
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
 
-    if(blockLeft < 40 && blockLeft > 0 && characterTop >= 175){
+
+    if(viewportWidth <= 750){
+
+        if(blockLeft < 20 && blockLeft > 0 && characterTop >= 90){
         
-        block.style.animation = "none";
-        block.style.display = "none";
-        running = false;
-        
-        text.innerText = "You Lose";
-        setTimeout(function(){
-            text.innerText = "Let's Play!"
-        }, 3000)
+            block.style.animation = "none";
+            block.style.display = "none";
+            running = false;
+            
+            text.innerText = "You Lose";
+            setTimeout(function(){
+                text.innerText = "Let's Play!"
+            }, 3000)
+        }
     }
+    else{
+        if(blockLeft < 40 && blockLeft > 0 && characterTop >= 175){
+        
+            block.style.animation = "none";
+            block.style.display = "none";
+            running = false;
+            
+            text.innerText = "You Lose";
+            setTimeout(function(){
+                text.innerText = "Let's Play!"
+            }, 3000)
+        }
+
+    }
+    
 
 }, 10)
 
@@ -45,3 +66,23 @@ function restart(){
     location.reload();
     setTimeout(function(){running = true;},10);
 }
+
+
+
+
+function animationTime(){
+    var time = Math.random(1, 2).toFixed(1);
+    
+    block.style.animation = "block" + time +"linear";
+
+}
+
+var time = 0;
+setInterval(function(){
+    if(running){
+        time++;
+        score.innerText = "score " + time
+    }
+
+}, 1500)
+    
